22fb352729c5c02a86a74c3913b9ede53e77e6d0
Alexandre Girbal <alexandre.girbal@loxsolution.com>
2023-02-21 17:09:19 +0100

    add custom colors to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
     content: ["./src/**/*.{ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#094067",
+                secondary: "#90B4CE",
+                tertiary: "#EF4565",
+                button: "#3DA9FC",
+                paragraph: "#5F6C7B",
+            },
+        },
     },
     plugins: [],
 };

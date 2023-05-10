2923d7d2b91b07be29c9f270a10b2c1e01cf5657
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2023-05-10 09:23:45 +0200

    single post static page

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,16 +1,18 @@
 import Head from "next/head";
 import PrivatePost from "../../modules/post/components/PrivatePost";
 import type { Post } from "@prisma/client";
+import { findManyPosts, findOnePost } from "../../modules/post/dao/find";
 
 type PostParams = {
     id: string;
 };
 
 export default function PostPage({ post }: { post: Post }) {
+    const headTitle = `RemplaMed | ${post.title}`;
     return (
         <>
             <Head>
-                <title>RemplaMed | {post.id}</title>
+                <title>{headTitle}</title>
                 <meta name="description" content={post.authorId} />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
@@ -20,40 +22,19 @@ export default function PostPage({ post }: { post: Post }) {
     );
 }
 
-export function getStaticPaths() {
+export async function getStaticPaths() {
+    const posts = await findManyPosts({ select: { id: true } });
     return {
-        paths: [
-            // can get paths from database
-            { params: { id: "1A" } },
-            { params: { id: "2B" } },
-            { params: { id: "3C" } },
-        ],
+        paths: posts.map((post) => ({ params: post })),
         fallback: false,
     };
 }
 
-export function getStaticProps({ params }: { params: PostParams }) {
-    const post: Post = {
-        id: params.id,
-        authorId: "John Doe",
-        message:
-            "I am the content of the post.\n Here is my phone number: 0612345678. This is from the dynamic route.",
-        createdAt: new Date(),
-        updatedAt: null,
-        published: false,
-        title: "",
-        availablityFrom: new Date("2023-09-01"),
-        availablityTo: new Date("2023-12-31"),
-        postalCode: "34000",
-        views: 0,
-    };
+export async function getStaticProps({ params }: { params: PostParams }) {
+    const post = await findOnePost(params.id);
     return {
         props: {
-            post: {
-                ...post,
-                createdAt: post.createdAt.toISOString(),
-                updatedAt: post.updatedAt?.toISOString() ?? post.updatedAt,
-            },
+            post,
         },
     };
 }

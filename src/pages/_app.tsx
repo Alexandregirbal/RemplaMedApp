d2c38a7cea821161d5444488f671827e7e13b7c8
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2023-05-25 15:11:58 +0200

    use redux Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,18 +4,23 @@ import type { AppType } from "next/app";
 import Layout from "../layout";
 import "styles/globals.css";
 import { wrapper } from "store";
+import { Provider } from "react-redux";
 
 const MyApp: AppType<{ session: Session | null }> = ({
     Component,
     pageProps: { session, ...pageProps },
 }) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { store, props } = wrapper.useWrappedStore(pageProps);
     return (
         <SessionProvider session={session}>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
+            <Provider store={store}>
+                <Layout>
+                    <Component {...props} />
+                </Layout>
+            </Provider>
         </SessionProvider>
     );
 };
 
-export default wrapper.withRedux(MyApp);
+export default MyApp;

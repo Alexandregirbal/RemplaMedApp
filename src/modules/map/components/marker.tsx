cc6f1fb981468e216b0b2a4360ebd3d22ca9ea4f
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2023-05-29 15:58:38 +0200

    Select on hover and router push on click

--- src/modules/map/components/marker.tsx
+++ src/modules/map/components/marker.tsx
@@ -3,6 +3,7 @@ import { type MapboxEvent, Marker } from "react-map-gl";
 import Pin from "./pin";
 import { useDispatch } from "react-redux";
 import { setSelectedPost } from "store/slices/posts/slice";
+import { useRouter } from "next/router";
 
 type MarkerComponentProps = {
     post: PostWithAuthorName;
@@ -10,24 +11,36 @@ type MarkerComponentProps = {
 
 const CustomMarkerComponent = ({ post }: MarkerComponentProps) => {
     const dispatch = useDispatch();
+    const { push } = useRouter();
 
-    const handleMarkerClick = (event: MapboxEvent<MouseEvent>) => {
+    const handleMarkerClick = async (event: MapboxEvent<MouseEvent>) => {
         event.originalEvent.stopPropagation(); // avoid `closeOnClick: true` on the Popup
         dispatch(setSelectedPost(post));
+        await push(`/posts/${post.id}`);
+    };
+
+    const handleMouseEnter = () => {
+        dispatch(setSelectedPost(post));
+    };
+
+    const handleMouseLeave = () => {
+        dispatch(setSelectedPost(null));
     };
 
     if (!post.latitude || !post.longitude) {
         return null;
     }
     return (
-        <Marker
-            longitude={post.longitude}
-            latitude={post.latitude}
-            anchor="bottom"
-            onClick={handleMarkerClick}
-        >
-            <Pin />
-        </Marker>
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <Marker
+                longitude={post.longitude}
+                latitude={post.latitude}
+                anchor="bottom"
+                onClick={handleMarkerClick}
+            >
+                <Pin />
+            </Marker>
+        </div>
     );
 };
 

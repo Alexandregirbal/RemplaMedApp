e4947781530233620b81374d77d6118046749ece
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2023-06-03 00:29:26 +0200

    Revert "Add Stepper"
    This reverts commit 34f8fc220459a21efa0bd24e956f0f00bac33782.

--- src/pages/posts/create.tsx
+++ src/pages/posts/create.tsx
@@ -1,39 +1,42 @@
-import Stepper from "components/UI/Stepper";
 import CreatePost from "modules/post/components/CreatePost";
 import { useState } from "react";
 
 enum Steps {
     CREATE_POST = "CREATE_POST",
     PREVIEW = "PREVIEW",
+    METADATA = "METADATA",
     PAYMENT = "PAYMENT",
     SUCCESS = "SUCCESS",
 }
 
-const steps: Steps[] = [
-    Steps.CREATE_POST,
-    Steps.PREVIEW,
-    Steps.PAYMENT,
-    Steps.SUCCESS,
-];
-
 export default function CreatePostPage() {
-    const [activeStep, setActiveStep] = useState<string>(Steps.CREATE_POST);
+    const [step, setStep] = useState<string>(Steps.CREATE_POST);
 
-    const handleStepChange = (step: string) => {
-        setActiveStep(step);
+    const handleStepChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        console.log(event.target.value);
+
+        setStep(event.target.value);
     };
 
     return (
         <div className="row flex h-full flex-col px-60">
-            <Stepper
-                steps={steps}
-                activeStep={activeStep}
-                handleStepClick={handleStepChange}
-            />
-            {activeStep === Steps.CREATE_POST && <CreatePost />}
-            {activeStep === Steps.PREVIEW && "I am the preview page"}
-            {activeStep === Steps.PAYMENT && "I am the payment page"}
-            {activeStep === Steps.SUCCESS && "I am the success page"}
+            <select
+                name="step"
+                id="stepSelect"
+                value={step}
+                onChange={handleStepChange}
+            >
+                {Object.keys(Steps).map((step) => (
+                    <option key={step} value={step}>
+                        {step}
+                    </option>
+                ))}
+            </select>
+            {step === Steps.CREATE_POST && <CreatePost />}
+            {step === Steps.PREVIEW && "I am the preview page"}
+            {step === Steps.METADATA && "I am the metadata page"}
+            {step === Steps.PAYMENT && "I am the payment page"}
+            {step === Steps.SUCCESS && "I am the success page"}
         </div>
     );
 }

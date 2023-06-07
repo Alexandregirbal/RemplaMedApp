bce2a226d151ada3e53227e02cd9f1c3febf80e7
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2023-06-07 12:30:11 +0200

    Message on Geolocation deactivated

--- src/modules/map/index.tsx
+++ src/modules/map/index.tsx
@@ -18,6 +18,8 @@ type MapComponentProps = {
 const MapComponent = ({ posts }: MapComponentProps) => {
     const dispatch = useDispatch();
 
+    const [isGeolocationAvailable, setIsGeolocationAvailable] =
+        useState<boolean>(true);
     const [viewport, setViewport] = useState<ViewState>({
         latitude: 46.77177190772532, // Bruère-Allichamps
         longitude: 2.4338675388986273, // Bruère-Allichamps
@@ -31,16 +33,23 @@ const MapComponent = ({ posts }: MapComponentProps) => {
             right: 0,
         },
     });
-
     useEffect(() => {
-        navigator.geolocation.getCurrentPosition((pos) => {
-            setViewport({
-                ...viewport,
-                latitude: pos.coords.latitude,
-                longitude: pos.coords.longitude,
-                zoom: 10,
-            });
-        });
+        navigator.geolocation.getCurrentPosition(
+            (pos) => {
+                setIsGeolocationAvailable(true);
+                setViewport({
+                    ...viewport,
+                    latitude: pos.coords.latitude,
+                    longitude: pos.coords.longitude,
+                    zoom: 10,
+                });
+            },
+            (error) => {
+                if (error.code === 1) {
+                    setIsGeolocationAvailable(false);
+                }
+            }
+        );
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
@@ -53,27 +62,38 @@ const MapComponent = ({ posts }: MapComponentProps) => {
     };
 
     return (
-        <Map
-            mapboxAccessToken="pk.eyJ1IjoiYWxleGFuZHJlZ2lyYmFsIiwiYSI6ImNsaHc2cHBmNjBndDkzZXF3dGM2ODh1c3YifQ.AhMdlbtUvHC2ucOOwRwsYw"
-            initialViewState={viewport}
-            mapStyle="mapbox://styles/mapbox/streets-v12"
-            latitude={viewport.latitude}
-            longitude={viewport.longitude}
-            zoom={viewport.zoom}
-            onMove={handleMapMove}
-            onClick={handleMapClick}
-            // viewState={{ ...viewport, width: 100, height: 100 }}
-        >
-            <GeolocateControl
-                position="top-left"
-                trackUserLocation
-                showUserLocation
-            />
-            <NavigationControl position="top-left" />
-            {posts.map((post) => (
-                <CustomMarkerComponent key={`marker-${post.id}`} post={post} />
-            ))}
-        </Map>
+        <>
+            {!isGeolocationAvailable && (
+                <div className="relative top-0 left-0 px-2 text-tertiary">
+                    {
+                        "La géolocalisation n'est pas disponible. Activez la, puis rafraîchissez la page pour une meilleure expérience."
+                    }
+                </div>
+            )}
+            <Map
+                mapboxAccessToken="pk.eyJ1IjoiYWxleGFuZHJlZ2lyYmFsIiwiYSI6ImNsaHc2cHBmNjBndDkzZXF3dGM2ODh1c3YifQ.AhMdlbtUvHC2ucOOwRwsYw"
+                initialViewState={viewport}
+                mapStyle="mapbox://styles/mapbox/streets-v12"
+                latitude={viewport.latitude}
+                longitude={viewport.longitude}
+                zoom={viewport.zoom}
+                onMove={handleMapMove}
+                onClick={handleMapClick}
+            >
+                <GeolocateControl
+                    position="top-left"
+                    trackUserLocation
+                    showUserLocation
+                />
+                <NavigationControl position="top-left" />
+                {posts.map((post) => (
+                    <CustomMarkerComponent
+                        key={`marker-${post.id}`}
+                        post={post}
+                    />
+                ))}
+            </Map>
+        </>
     );
 };
 

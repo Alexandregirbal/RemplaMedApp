dba37079b640866b963880d70df82f172d6b4a32
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2023-07-04 17:35:00 +0200

    add header icon dropdown

--- src/modules/auth/components/LoggedIcon.tsx
+++ src/modules/auth/components/LoggedIcon.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { signOut } from "next-auth/react";
-import Link from "next/link";
 import UserIcon from "modules/user/components/userIcon";
+import { signOut } from "next-auth/react";
+import { useState } from "react";
 import type { UserSession } from "../types/user";
+import Link from "next/link";
 
 type LoggedProps = {
     user: UserSession;
@@ -14,6 +15,8 @@ export default function Logged({ user }: LoggedProps) {
         ? user.name?.charAt(0).toUpperCase() +
           user.name?.charAt(1).toUpperCase()
         : user.name;
+
+    const [dropdown, setDropdown] = useState(false);
 
     const handleSignout = () => {
         console.log("Signing out...");
@@ -26,33 +29,44 @@ export default function Logged({ user }: LoggedProps) {
             });
     };
 
+    const toggleDropdow = () => {
+        setDropdown(!dropdown);
+    };
+
     return (
-        <li className="flex items-center gap-8">
-            <button
-                className="rounded-md bg-gray-700 px-6 py-2 text-sm text-white "
-                onClick={() => handleSignout()}
-            >
-                Déconnexion
-            </button>
-            {user && (
-                <Link href={"/users/me"}>
+        <div className="relative flex items-center gap-8">
+            <div onClick={toggleDropdow}>
+                <div
+                    style={{
+                        width: "50px",
+                        height: "50px",
+                        lineHeight: "50px",
+                        fontSize: "20px",
+                    }}
+                    className="rounded-full bg-primary text-center text-white shadow-xl hover:cursor-pointer"
+                >
                     {user.image ? (
                         <UserIcon image={user.image} size={50} />
                     ) : (
-                        <div
-                            style={{
-                                width: "50px",
-                                height: "50px",
-                                lineHeight: "50px",
-                                fontSize: "20px",
-                            }}
-                            className="rounded-full bg-primary text-center text-white shadow-xl"
-                        >
-                            {initials}
-                        </div>
+                        initials
                     )}
-                </Link>
-            )}
-        </li>
+                </div>
+
+                {dropdown && (
+                    <ul className="absolute right-0 z-10 mt-2 flex flex-col justify-evenly gap-2 rounded-md border bg-background p-2 shadow-sm">
+                        <li>
+                            <Link href="users/me">Mon profile</Link>
+                        </li>
+                        <hr />
+                        <li
+                            className="hover:cursor-pointer"
+                            onClick={handleSignout}
+                        >
+                            Déconnexion
+                        </li>
+                    </ul>
+                )}
+            </div>
+        </div>
     );
 }

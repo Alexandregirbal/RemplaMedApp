f1a93be1bb126bc74b9d38de2d1bdc1d2054ad94
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2023-07-05 17:28:16 +0200

    Better icon to publish

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -2,6 +2,7 @@ import { Tooltip } from "flowbite-react";
 import Logged from "modules/auth/components/LoggedIcon";
 import Login from "modules/auth/components/LoginButton";
 import { getMetaData } from "modules/post/services/getMetadata";
+import AddIcon from "modules/ui/icons/add";
 import { type NextPage } from "next";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
@@ -9,10 +10,12 @@ import Link from "next/link";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectPostsState, setPostsMetadata } from "store/slices/posts/slice";
+import { selectUIState } from "store/slices/ui/slice";
 
 const Header: NextPage = () => {
     const { data: session } = useSession();
     const { metadata } = useSelector(selectPostsState);
+    const { isMobile } = useSelector(selectUIState);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -42,27 +45,37 @@ const Header: NextPage = () => {
                                 : "Connectez vous pour créer un poste"
                         }
                     >
-                        <Link href={session ? "/posts/create" : "/"}>
-                            <button
-                                className={`h-10 rounded-lg border px-8 ${
-                                    session
-                                        ? "border-cta  text-cta outline-cta"
-                                        : "border-gray-500  text-gray-500 outline-gray-500 hover:cursor-default"
-                                }`}
-                            >
-                                + Publier
-                            </button>
+                        <Link
+                            href={session ? "/posts/create" : "/"}
+                            className={`flex items-center gap-2 ${
+                                session
+                                    ? " text-cta "
+                                    : " text-gray-500 hover:cursor-default"
+                            }`}
+                        >
+                            <AddIcon size={45} />
+                            {!isMobile && (
+                                <span className="text-xl">{"Publier"}</span>
+                            )}
                         </Link>
                     </Tooltip>
                 </div>
-                <div id="posts-number" className="text-xl">
-                    Plus de {metadata.totalRecentPosts} annonces récentes
+                <div
+                    id="headline"
+                    className=" flex h-8 flex-col justify-center "
+                >
+                    <span className="flex justify-center text-3xl">
+                        {"RemplaMed"}
+                    </span>
+                    <span className="flex justify-center">
+                        {"Réseau de remplacement infirmier"}
+                    </span>
                 </div>
 
                 {session ? <Logged user={session.user} /> : <Login />}
             </nav>
-            <div id="headline" className=" flex h-8 justify-center text-3xl">
-                RemplaMed, réseau de remplacement infirmier
+            <div id="posts-number" className="flex justify-center text-xl">
+                Plus de {metadata.totalRecentPosts} annonces récentes
             </div>
         </div>
     );

a33ffa1619c305fbbc2b8d2e003be36ec9877f14
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2023-09-05 15:22:47 +0200

    add policies

---
+++ src/pages/policy/privacy.tsx
@@ -0,0 +1,189 @@
+import Head from "next/head";
+
+const PrivacyPolicy = () => {
+    return (
+        <>
+            <Head>
+                <meta
+                    http-equiv="Content-Type"
+                    content="text/html; charset=utf-8"
+                />
+                <title>RemplaMed Privacy Policy</title>
+            </Head>
+            <article
+                id="82b88ffe-72ac-4384-9b3b-7d57ee1d1546"
+                className="page sans"
+            >
+                <div className="page-body">
+                    <p id="0e8b5b08-12dd-46dc-a121-e5a518869346" className="">
+                        Effective Date: 2023-09-01
+                    </p>
+                    <hr id="34531079-8995-4755-8e8a-23ab9effefd4" />
+                    <p id="d4b60177-1f3e-4ed0-ac74-0eabaad89b37" className="">
+                        This Privacy Policy outlines how RemplaMed collects,
+                        uses, maintains, and protects the personal information
+                        of users of the RemplaMed web application. This policy
+                        applies solely to information collected through the App
+                        and explains the procedures we have in place to ensure
+                        your privacy and the security of your personal
+                        information.
+                    </p>
+                    <p id="5515e0c2-69c1-4b1c-86d4-9066cb304d79" className="">
+                        <strong>Information We Collect</strong>
+                    </p>
+                    <p id="6f326f08-b974-42d1-aa8e-691c1c68dd62" className="">
+                        We collect certain information when you use the
+                        RemplaMed app:
+                    </p>
+                    <ol
+                        type="1"
+                        id="c1f79af7-93cb-45c9-beed-225858affc5c"
+                        className="numbered-list"
+                        start={1}
+                    >
+                        <li>
+                            <strong>Google Account Information:</strong> In
+                            order to use RemplaMed, you can authenticate via
+                            your Google account. We collect your basic profile
+                            information from your Google account, which includes
+                            your name, email address, and profile picture.
+                        </li>
+                    </ol>
+                    <ol
+                        type="1"
+                        id="d83057e1-70a7-4e01-b7d3-00615df932dd"
+                        className="numbered-list"
+                        start={2}
+                    >
+                        <li>
+                            <strong>Content:</strong> When you interact with
+                            RemplaMed, you may post content, such as text. This
+                            content may be associated with your Google account
+                            and may be visible to other users of RemplaMed.
+                        </li>
+                    </ol>
+                    <ol
+                        type="1"
+                        id="9dde4c64-aa27-4510-b645-2ba0e3a381ad"
+                        className="numbered-list"
+                        start={3}
+                    >
+                        <li>
+                            <strong>Usage Data:</strong> We automatically
+                            collect certain information about your interaction
+                            with RemplaMed, including the actions you take, the
+                            features you use.
+                        </li>
+                    </ol>
+                    <p id="1613f1c7-e8d4-4d8f-946d-4f62d77ed9aa" className="">
+                        <strong>How We Use Your Information</strong>
+                    </p>
+                    <p id="96a7ed13-a405-4314-848a-e4e13c6ab227" className="">
+                        We use the information collected to provide and improve
+                        our services, including:
+                    </p>
+                    <ol
+                        type="1"
+                        id="07b29cd0-61e0-4db8-8d69-f5cd37cff38b"
+                        className="numbered-list"
+                        start={1}
+                    >
+                        <li>
+                            <strong>Authentication:</strong> Your Google account
+                            information is used to verify your identity and
+                            provide you access to RemplaMed.
+                        </li>
+                    </ol>
+                    <ol
+                        type="1"
+                        id="8533e0cf-dcab-4eb5-bab6-f3b396d13d16"
+                        className="numbered-list"
+                        start={2}
+                    >
+                        <li>
+                            <strong>User-generated Content:</strong> The content
+                            you post on RemplaMed is used to display your
+                            contributions to other users and to enhance the
+                            overall user experience.
+                        </li>
+                    </ol>
+                    <ol
+                        type="1"
+                        id="1144c6ce-1705-4318-973a-98753a6f0164"
+                        className="numbered-list"
+                        start={3}
+                    >
+                        <li>
+                            <strong>App Improvement:</strong> We analyze usage
+                            data to better understand how users interact with
+                            RemplaMed, identify areas for improvement, and
+                            develop new features.
+                        </li>
+                    </ol>
+                    <ol
+                        type="1"
+                        id="486f8e77-afd4-4211-8f15-0146696a4060"
+                        className="numbered-list"
+                        start={4}
+                    >
+                        <li>
+                            <strong>Communication:</strong> We may use your
+                            email address to communicate important updates,
+                            notifications, or changes related to RemplaMed.
+                        </li>
+                    </ol>
+                    <p id="2e5b8bea-5a31-4769-b7eb-fb7f7f5eb8e1" className="">
+                        <strong>Data Security</strong>
+                    </p>
+                    <p id="d49f8472-c8b3-4775-bff0-d9ccb6875b65" className="">
+                        We take data security seriously and implement
+                        appropriate technical and organizational measures to
+                        protect your information from unauthorized access,
+                        alteration, disclosure, or destruction.
+                    </p>
+                    <p id="18096f68-a490-4e66-bfba-660bfc46fbd7" className="">
+                        <strong>Third-party Access</strong>
+                    </p>
+                    <p id="63d5c10f-cebe-4e2c-bc56-d928bf53e24b" className="">
+                        We do not share your personal information with third
+                        parties except as described in this policy. Your Google
+                        account information is used solely for authentication
+                        purposes and is not shared with any third-party service
+                        providers.
+                    </p>
+                    <p id="947960b8-c6d2-4dc6-b42b-64a966f9f8d1" className="">
+                        <strong>Your Choices</strong>
+                    </p>
+                    <p id="aa6332bf-267b-4408-85a4-acb7c2b2a4f2" className="">
+                        You can control the information you provide to us by
+                        managing your Google account settings. You can choose
+                        not to provide certain information, but this may limit
+                        your access to certain features of RemplaMed.
+                    </p>
+                    <p id="201fc29c-2446-43fa-be15-6f579b37818a" className="">
+                        <strong>Changes to this Privacy Policy</strong>
+                    </p>
+                    <p id="7295c3a9-b61c-4e8c-8496-621745d0a7d8" className="">
+                        We may update this Privacy Policy from time to time. Any
+                        changes will be communicated via email.
+                    </p>
+                    <p id="d655a7a6-0345-4352-82dc-f7e0700fdeb9" className="">
+                        <strong>Contact Us</strong>
+                    </p>
+                    <p id="97ca819c-a8ae-4689-bace-15d639e2e132" className="">
+                        If you have any questions, concerns, or requests related
+                        to your personal information or this Privacy Policy,
+                        please contact us at our contact email provided on
+                        rempla-med.fr/contact.
+                    </p>
+                    <p id="badf7752-d0e2-404e-9ce9-d85710b36cf2" className="">
+                        By using the RemplaMed app, you agree to the terms
+                        outlined in this Privacy Policy.
+                    </p>
+                </div>
+            </article>
+        </>
+    );
+};
+
+export default PrivacyPolicy;
---
+++ src/pages/policy/service.tsx
@@ -0,0 +1,122 @@
+import Head from "next/head";
+
+const ServicePolicy = () => {
+    return (
+        <>
+            <Head>
+                <meta
+                    http-equiv="Content-Type"
+                    content="text/html; charset=utf-8"
+                />
+                <title>RemplaMed Terms of Service</title>
+            </Head>
+            <article
+                id="f3232f34-fc11-437e-9ccf-18e1fc843d7a"
+                className="page sans"
+            >
+                <div className="page-body">
+                    <h1 id="5b1a0fa6-9a0b-4369-b630-45c98eced77f" className="">
+                        RemplaMed Terms of Service
+                    </h1>
+                    <p id="db5247d2-5e7f-4788-bd86-1cc6de73e848" className="">
+                        By using the RemplaMed web application, you agree to the
+                        following terms and conditions:
+                    </p>
+                    <p id="f1febc92-1e9e-4446-a13e-efefb45b4ab0" className="">
+                        <strong>1. Use of RemplaMed</strong>
+                    </p>
+                    <p id="15e3d422-ab6b-4431-9368-c41aa9884ff4" className="">
+                        RemplaMed is provided solely for your personal use. You
+                        may not use RemplaMed for any commercial purpose, unless
+                        you have received written permission from RemplaMed. You
+                        may not use RemplaMed to violate any applicable laws or
+                        regulations.
+                    </p>
+                    <p id="f3f1a948-e61a-43a5-84b7-fd2f565f33e7" className="">
+                        <strong>2. User-generated Content</strong>
+                    </p>
+                    <p id="2b754d1f-d428-4108-ba3f-089fe73104af" className="">
+                        You are solely responsible for the content you post on
+                        RemplaMed. You represent and warrant that you have all
+                        necessary rights to post such content and that such
+                        content does not violate any applicable laws or
+                        regulations. By posting content on RemplaMed, you grant
+                        RemplaMed a non-exclusive, transferable, sub-licensable,
+                        royalty-free, worldwide license to use, reproduce,
+                        modify, create derivative works based on, distribute,
+                        publicly display, publicly perform, and otherwise
+                        exploit in any manner such content in all formats and
+                        distribution channels now known or hereafter devised.
+                    </p>
+                    <p id="f976a279-2435-42bc-8542-ba0e0e923d1d" className="">
+                        <strong>3. Intellectual Property</strong>
+                    </p>
+                    <p id="0b05a322-8a62-4905-8f99-42ceb7741df2" className="">
+                        The RemplaMed app and all content and other materials
+                        available through RemplaMed, including, but not limited
+                        to, trademarks, logos, service marks, and trade dress
+                        (collectively, the &quot;RemplaMed Content&quot;) are
+                        owned by RemplaMed or its licensors and are protected by
+                        copyright, trademark, and other intellectual property
+                        laws. You may not copy, modify, distribute, sell, or
+                        lease any part of RemplaMed or the RemplaMed Content.
+                    </p>
+                    <p id="d112963d-d9d9-4d1f-9e54-efbcaa418a18" className="">
+                        <strong>4. Limitation of Liability</strong>
+                    </p>
+                    <p id="5f3aefbb-03ec-4fc4-9ac2-667ce0c310bd" className="">
+                        RemplaMed will not be liable to you for any indirect,
+                        incidental, special, consequential, or punitive damages
+                        arising out of your use of RemplaMed or the RemplaMed
+                        Content. RemplaMed&#x27;s liability to you for any
+                        direct damages will be limited to the amount paid by
+                        you, if any, to access or use RemplaMed.
+                    </p>
+                    <p id="d38ce913-9a83-4fb2-9940-c1b76756e16b" className="">
+                        <strong>5. Disclaimer of Warranties</strong>
+                    </p>
+                    <p id="d2bddf7d-e2bc-4022-9190-75928c1948d2" className="">
+                        RemplaMed provides RemplaMed and the RemplaMed Content
+                        &quot;as is&quot; and without any warranty or condition,
+                        express, implied, or statutory. RemplaMed specifically
+                        disclaims any implied warranties of merchantability,
+                        fitness for a particular purpose, non-infringement, and
+                        any warranties arising out of course of dealing or usage
+                        of trade.
+                    </p>
+                    <p id="9aa77315-b9e6-4532-a5de-88e23953d321" className="">
+                        <strong>6. Governing Law and Dispute Resolution</strong>
+                    </p>
+                    <p id="831f2f25-8a37-4e89-8301-07cdd0b92307" className="">
+                        These Terms of Service will be governed by and construed
+                        in accordance with the laws of France, without giving
+                        effect to any principles of conflicts of law. Any
+                        dispute arising out of or relating to these Terms of
+                        Service or your use of RemplaMed will be resolved
+                        exclusively in the courts of France.
+                    </p>
+                    <p id="05c4be86-26c3-49ae-aab9-9b5ea7a7723b" className="">
+                        <strong>7. Changes to these Terms of Service</strong>
+                    </p>
+                    <p id="e2d24c9f-13ff-4620-9784-df0b87dc77f0" className="">
+                        We may update these Terms of Service from time to time.
+                        Any changes will be communicated via email.
+                    </p>
+                    <p id="f9818b4c-076a-40da-92d3-d5ec2ec183dc" className="">
+                        <strong>Contact Us</strong>
+                    </p>
+                    <p id="66cf0ce4-611f-4246-843a-7253bba6887b" className="">
+                        If you have any questions, concerns, or requests related
+                        to these Terms of Service or your use of RemplaMed,
+                        please contact us at our contact email provided on{" "}
+                        <a href="http://rempla-med.fr/contact">
+                            rempla-med.fr/contact
+                        </a>
+                        .
+                    </p>
+                </div>
+            </article>
+        </>
+    );
+};
+export default ServicePolicy;

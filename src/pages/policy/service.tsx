330574961c17d9fd12c832e9f10122a4df6ca59d
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2023-10-19 18:05:12 +0200

    update services

--- src/pages/policy/service.tsx
+++ src/pages/policy/service.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 const ServicePolicy = () => {
     return (
@@ -8,113 +9,110 @@ const ServicePolicy = () => {
                     http-equiv="Content-Type"
                     content="text/html; charset=utf-8"
                 />
-                <title>RemplaMed Terms of Service</title>
+                <title>{"RemplaMed | Conditions d'utilisation"}</title>
             </Head>
-            <article
-                id="f3232f34-fc11-437e-9ccf-18e1fc843d7a"
-                className="page sans"
-            >
-                <div className="page-body">
-                    <h1 id="5b1a0fa6-9a0b-4369-b630-45c98eced77f" className="">
-                        RemplaMed Terms of Service
-                    </h1>
-                    <p id="db5247d2-5e7f-4788-bd86-1cc6de73e848" className="">
-                        By using the RemplaMed web application, you agree to the
-                        following terms and conditions:
-                    </p>
-                    <p id="f1febc92-1e9e-4446-a13e-efefb45b4ab0" className="">
-                        <strong>1. Use of RemplaMed</strong>
-                    </p>
-                    <p id="15e3d422-ab6b-4431-9368-c41aa9884ff4" className="">
-                        RemplaMed is provided solely for your personal use. You
-                        may not use RemplaMed for any commercial purpose, unless
-                        you have received written permission from RemplaMed. You
-                        may not use RemplaMed to violate any applicable laws or
-                        regulations.
-                    </p>
-                    <p id="f3f1a948-e61a-43a5-84b7-fd2f565f33e7" className="">
-                        <strong>2. User-generated Content</strong>
-                    </p>
-                    <p id="2b754d1f-d428-4108-ba3f-089fe73104af" className="">
-                        You are solely responsible for the content you post on
-                        RemplaMed. You represent and warrant that you have all
-                        necessary rights to post such content and that such
-                        content does not violate any applicable laws or
-                        regulations. By posting content on RemplaMed, you grant
-                        RemplaMed a non-exclusive, transferable, sub-licensable,
-                        royalty-free, worldwide license to use, reproduce,
-                        modify, create derivative works based on, distribute,
-                        publicly display, publicly perform, and otherwise
-                        exploit in any manner such content in all formats and
-                        distribution channels now known or hereafter devised.
-                    </p>
-                    <p id="f976a279-2435-42bc-8542-ba0e0e923d1d" className="">
-                        <strong>3. Intellectual Property</strong>
-                    </p>
-                    <p id="0b05a322-8a62-4905-8f99-42ceb7741df2" className="">
-                        The RemplaMed app and all content and other materials
-                        available through RemplaMed, including, but not limited
-                        to, trademarks, logos, service marks, and trade dress
-                        (collectively, the &quot;RemplaMed Content&quot;) are
-                        owned by RemplaMed or its licensors and are protected by
-                        copyright, trademark, and other intellectual property
-                        laws. You may not copy, modify, distribute, sell, or
-                        lease any part of RemplaMed or the RemplaMed Content.
-                    </p>
-                    <p id="d112963d-d9d9-4d1f-9e54-efbcaa418a18" className="">
-                        <strong>4. Limitation of Liability</strong>
-                    </p>
-                    <p id="5f3aefbb-03ec-4fc4-9ac2-667ce0c310bd" className="">
-                        RemplaMed will not be liable to you for any indirect,
-                        incidental, special, consequential, or punitive damages
-                        arising out of your use of RemplaMed or the RemplaMed
-                        Content. RemplaMed&#x27;s liability to you for any
-                        direct damages will be limited to the amount paid by
-                        you, if any, to access or use RemplaMed.
-                    </p>
-                    <p id="d38ce913-9a83-4fb2-9940-c1b76756e16b" className="">
-                        <strong>5. Disclaimer of Warranties</strong>
-                    </p>
-                    <p id="d2bddf7d-e2bc-4022-9190-75928c1948d2" className="">
-                        RemplaMed provides RemplaMed and the RemplaMed Content
-                        &quot;as is&quot; and without any warranty or condition,
-                        express, implied, or statutory. RemplaMed specifically
-                        disclaims any implied warranties of merchantability,
-                        fitness for a particular purpose, non-infringement, and
-                        any warranties arising out of course of dealing or usage
-                        of trade.
-                    </p>
-                    <p id="9aa77315-b9e6-4532-a5de-88e23953d321" className="">
-                        <strong>6. Governing Law and Dispute Resolution</strong>
-                    </p>
-                    <p id="831f2f25-8a37-4e89-8301-07cdd0b92307" className="">
-                        These Terms of Service will be governed by and construed
-                        in accordance with the laws of France, without giving
-                        effect to any principles of conflicts of law. Any
-                        dispute arising out of or relating to these Terms of
-                        Service or your use of RemplaMed will be resolved
-                        exclusively in the courts of France.
-                    </p>
-                    <p id="05c4be86-26c3-49ae-aab9-9b5ea7a7723b" className="">
-                        <strong>7. Changes to these Terms of Service</strong>
-                    </p>
-                    <p id="e2d24c9f-13ff-4620-9784-df0b87dc77f0" className="">
-                        We may update these Terms of Service from time to time.
-                        Any changes will be communicated via email.
-                    </p>
-                    <p id="f9818b4c-076a-40da-92d3-d5ec2ec183dc" className="">
-                        <strong>Contact Us</strong>
-                    </p>
-                    <p id="66cf0ce4-611f-4246-843a-7253bba6887b" className="">
-                        If you have any questions, concerns, or requests related
-                        to these Terms of Service or your use of RemplaMed,
-                        please contact us at our contact email provided on{" "}
-                        <a href="http://rempla-med.fr/contact">
-                            rempla-med.fr/contact
-                        </a>
-                        .
-                    </p>
+            <article className="flex h-full flex-col gap-2 p-4">
+                <h1 className="text-center text-2xl">
+                    <strong>{"Conditions d'utilisation de RemplaMed"}</strong>
+                </h1>
+                <div className="flex flex-col gap-2">
+                    {
+                        "En utilisant l'application web RemplaMed, vous acceptez les conditions suivantes :"
+                    }
                 </div>
+                <p className="">
+                    <strong>1. Utilisation de RemplaMed</strong>
+                </p>
+                <p className="">
+                    RemplaMed est fourni uniquement pour votre usage personnel.
+                    Vous ne pouvez pas utiliser RemplaMed à des fins
+                    commerciales, sauf si vous avez obtenu une autorisation
+                    écrite de RemplaMed. Vous ne pouvez pas utiliser RemplaMed
+                    pour enfreindre les lois ou règlements applicables.
+                </p>
+                <p className="">
+                    <strong>{"2. Contenu généré par l'utilisateur"}</strong>
+                </p>
+                <p className="">
+                    Vous êtes seul responsable du contenu que vous publiez sur
+                    RemplaMed. Vous déclarez et garantissez que vous avez tous
+                    les droits nécessaires pour publier un tel contenu et que ce
+                    contenu ne viole pas les lois ou règlements applicables. En
+                    publiant du contenu sur RemplaMed, vous accordez à RemplaMed
+                    une licence non exclusive, cessible, sous-licenciable, sans
+                    redevance et mondiale pour utiliser, reproduire, modifier,
+                    créer des œuvres dérivées, distribuer, afficher
+                    publiquement, exécuter publiquement et exploiter de toute
+                    autre manière ce contenu dans tous les formats et canaux de
+                    distribution connus ou développés ultérieurement.
+                </p>
+                <p className="">
+                    <strong>3. Propriété intellectuelle</strong>
+                </p>
+                <p className="">
+                    {`L'application RemplaMed et tout le contenu et autres
+                        matériaux disponibles via RemplaMed, y compris, mais
+                        sans s'y limiter, les marques de commerce, logos,
+                        marques de service et habillage commercial
+                        (collectivement, le "Contenu RemplaMed"), sont la
+                        propriété de RemplaMed ou de ses concédants de licence
+                        et sont protégés par le droit d'auteur, les marques de
+                        commerce et d'autres lois sur la propriété
+                        intellectuelle. Vous ne pouvez pas copier, modifier,
+                        distribuer, vendre ou louer une partie de RemplaMed ou
+                        du Contenu RemplaMed.`}
+                </p>
+                <p className="">
+                    <strong>4. Limitation de responsabilité</strong>
+                </p>
+                <p className="">
+                    RemplaMed ne sera pas responsable envers vous pour des
+                    dommages indirects, accessoires, spéciaux, consécutifs ou
+                    punitifs découlant de votre utilisation de RemplaMed ou du
+                    Contenu RemplaMed. La responsabilité de RemplaMed envers
+                    vous pour des dommages directs sera limitée au montant payé
+                    par vous, le cas échéant, pour accéder ou utiliser
+                    RemplaMed.
+                </p>
+                <p className="">
+                    <strong>5. Exclusion de garanties</strong>
+                </p>
+                <p className="">
+                    {
+                        "RemplaMed fournit RemplaMed et le Contenu RemplaMed tel quel et sans aucune garantie ou condition, expresse, implicite ou légale. RemplaMed décline expressément toute garantie implicite de qualité marchande, d'adéquation à un usage particulier, de non-contrefaçon et toute garantie découlant de la pratique commerciale ou de l'usage du commerce."
+                    }
+                </p>
+                <p className="">
+                    <strong>6. Loi applicable et règlement des litiges</strong>
+                </p>
+                <p className="">
+                    {
+                        "Ces Conditions d'utilisation seront régies par et interprétées conformément aux lois de la France, sans donner effet à aucun principe de conflit de lois. Tout litige découlant de ou lié à ces Conditions d'utilisation ou à votre utilisation de RemplaMed sera résolu exclusivement devant les tribunaux français."
+                    }
+                </p>
+                <p className="">
+                    <strong>
+                        {"7. Modifications de ces Conditions d'utilisation"}
+                    </strong>
+                </p>
+                <p className="">
+                    {
+                        "Nous pouvons mettre à jour ces Conditions d'utilisation de temps à autre. Toute modification sera communiquée par e-mail."
+                    }
+                </p>
+                <p className="text-xl">
+                    <strong>{"Contactez-nous"}</strong>
+                </p>
+                <p className="pb-10">
+                    {
+                        "Si vous avez des questions, des préoccupations ou des demandes concernant vos informations personnelles ou cette Politique de confidentialité, veuillez nous contacter à l'adresse e-mail de contact fournie sur "
+                    }
+                    <Link href="/contact" className="underline">
+                        {"rempal-med.fr/contact"}
+                    </Link>
+                    {"."}
+                </p>
+                <hr />
             </article>
         </>
     );

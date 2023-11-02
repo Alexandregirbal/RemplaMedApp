661dcced764c2447989c49bd13015031a6b9be6f
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2023-11-02 08:53:34 +0100

    Add privacy policy link on homepage

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 export default function Footer() {
     return (
-        <footer className="fixed flex h-6 w-full items-center justify-center bg-background">
-            <p className="text-sm text-gray-500">
-                {`© ${new Date().getFullYear()} RemplaMed`}
-            </p>
+        <footer className="fixed flex h-6 w-full items-center justify-around bg-background text-sm">
+            <Link href={"/about"}>{"À propos"}</Link>
+            <Link href={"/policy/privacy"}>
+                {"Politique de confidentialité"}
+            </Link>
         </footer>
     );
 }

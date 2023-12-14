93e6b82aa77b8945bea6da21889bc6d25ef04a27
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2023-12-14 22:31:02 +0100

    fix createdAt filter

--- src/modules/filters/services/filterByCreatedAt.ts
+++ src/modules/filters/services/filterByCreatedAt.ts
@@ -5,7 +5,7 @@ export const filterByCreatedAt = (
     posts: PostWithAuthorName[],
     createdAt: number
 ) => {
-    if (createdAt <= 0) return [];
+    if (createdAt <= 0) return posts;
 
     return posts.filter((post) =>
         dayjs(post.createdAt).isAfter(dayjs().subtract(createdAt, "hours"))

ea8b6d88cbc86fe25fad323356ceccc7161f2d39
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2024-01-11 17:58:13 +0100

    remove unnecessary env var

--- src/env.mjs
+++ src/env.mjs
@@ -5,12 +5,8 @@ import { z } from "zod";
  * This way you can ensure the app isn't built with invalid env vars.
  */
 const server = z.object({
-    POSTGRES_USER: z.string(),
-    POSTGRES_PASSWORD: z.string(),
     POSTGRES_DATABASE: z.string(),
     POSTGRES_PRISMA_URL: z.string().url(),
-    DATABASE_URL: z.string().url(),
-    POSTGRES_URL: z.string().url(),
     NODE_ENV: z.enum(["development", "test", "production"]),
     NEXTAUTH_SECRET:
         process.env.NODE_ENV === "production"
@@ -47,10 +43,6 @@ const client = z.object({
  */
 const processEnv = {
     NODE_ENV: process.env.NODE_ENV,
-    DATABASE_URL: process.env.DATABASE_URL,
-    POSTGRES_URL: process.env.POSTGRES_URL,
-    POSTGRES_USER: process.env.POSTGRES_USER,
-    POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
     POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
     POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,

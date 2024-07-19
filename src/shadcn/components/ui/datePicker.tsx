eef5ce4f8cc33e4a2b7d446607ec8316f1b332f5
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2024-07-19 18:57:55 +0200

    first setup

--- src/shadcn/components/ui/datePicker.tsx
+++ src/shadcn/components/ui/datePicker.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { format } from "date-fns";
+import dayjs from "dayjs";
 import { Calendar as CalendarIcon } from "lucide-react";
-import * as React from "react";
 
+import { fr } from "date-fns/locale";
 import { Button } from "shadcn/components/ui/button";
 import { Calendar } from "shadcn/components/ui/calendar";
 import {
@@ -12,30 +12,48 @@ import {
     PopoverTrigger,
 } from "shadcn/components/ui/popover";
 import { cn } from "shadcn/lib/utils";
+type DatePickerProps = {
+    className?: string;
+    onChange: (date?: Date) => void;
+    selected?: Date;
+    minDate?: Date | null;
+};
 
-export function DatePicker() {
-    const [date, setDate] = React.useState<Date>();
-
+export function DatePicker({
+    className,
+    onChange,
+    selected,
+    minDate,
+}: DatePickerProps) {
     return (
         <Popover>
             <PopoverTrigger asChild>
                 <Button
                     variant={"outline"}
                     className={cn(
-                        "w-[280px] justify-start text-left font-normal",
-                        !date && "text-muted-foreground"
+                        " w-full justify-start text-left font-normal",
+                        !selected && "text-muted-foreground",
+                        className
                     )}
                 >
                     <CalendarIcon className="mr-2 h-4 w-4" />
-                    {date ? format(date, "PPP") : <span>Pick a date</span>}
+                    {selected ? (
+                        dayjs(selected).format("D MMMM YYYY")
+                    ) : (
+                        <span>Choisir une date</span>
+                    )}
                 </Button>
             </PopoverTrigger>
             <PopoverContent className="w-auto p-0">
                 <Calendar
                     mode="single"
-                    selected={date}
-                    onSelect={setDate}
+                    selected={selected}
+                    onSelect={onChange}
                     initialFocus
+                    fromDate={minDate ? dayjs(minDate).toDate() : undefined}
+                    locale={fr}
+                    showOutsideDays={false}
+                    weekStartsOn={1}
                 />
             </PopoverContent>
         </Popover>
